bbea78dae5ddca80089f31b1ff5bfa31f4128df6
Rahmat Sayfuddin <rahmatsayfuddin@gmail.com>
2025-07-18 11:34:41 +0700

    Spi Project

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from products.models import Product
+from django.utils import timezone
 
 class Project(models.Model):
     STATUS_CHOICES = [
@@ -17,6 +18,33 @@ class Project(models.Model):
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='Planned')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='In Progress')
+    start_date = models.DateField()
+    end_date = models.DateField()  # deadline
+    completed_date = models.DateField(null=True, blank=True)
+
+    def spi(self):
+        if not self.end_date or not self.start_date:
+            return None
+
+        planned_duration = (self.end_date - self.start_date).days
+
+        if planned_duration <= 0:
+            return None
+
+        if self.completed_date:
+            actual_duration = (self.completed_date - self.start_date).days
+        else:
+            actual_duration = (timezone.now().date() - self.start_date).days
+
+        if actual_duration <= 0:
+            return None
+
+        spi_value = planned_duration / actual_duration
+        return round(spi_value, 2)
+
+
 
     def __str__(self):
         return self.project_name
--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Project
 from .forms import ProjectForm
 from products.models import Product
+from django.utils import timezone
 
 def project_list(request):
     projects = Project.objects.select_related('product').all()
@@ -11,14 +12,19 @@ def project_create(request, product_id=None):
     if request.method == 'POST':
         form = ProjectForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('project_list')
+            project = form.save(commit=False)
+
+            if project.status == 'Completed' and not project.completed_date:
+                project.completed_date = timezone.now().date()
+            elif project.status != 'Completed':
+                project.completed_date = None
+            project.save()
+            return redirect('project_list')  
     else:
         form = ProjectForm()
         if product_id:
             form.fields['product'].initial = product_id
-            form.fields['product'].disabled = True  # ⛔️ Biar tidak bisa diganti
-
+            form.fields['product'].disabled = True 
     return render(request, 'projects/project_form.html', {'form': form})
 
 def project_update(request, pk):
